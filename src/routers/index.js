8bad652b71c15615234ba81a13fe11baee22185d
Jason <gt07891@greattree.com.tw>
2021-12-10 09:26:52 +0800

    路由模式調整

--- src/routers/index.js
+++ src/routers/index.js
@@ -16,9 +16,6 @@ export const routes = [
   { path: "/:path(.*)", redirect: `/about` },
 ];
 
-const isProduction = process.env.NODE_ENV === "production";
-const rootPath = isProduction ? "/vue3Blog/" : "/";
-
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
